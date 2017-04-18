8eee5d1ac9ad0daf5434472111d882508ad3ba8c
theastrocat <brian.mcadams88@gmail.com>
2017-04-18 13:24:18 -0700

    Filled out reddit main scrape basic

--- src/main_top.py
+++ src/main_top.py
@@ -1,3 +1,31 @@
 """
 Module for scaping reddit top (front page).
 """
+import time
+from bs4 import BeautifulSoup
+from pymongo import MongoClient
+import datetime
+import random
+
+from retrieve_source import Scrape
+from reddit_scraping import Reddit_Scrape
+
+client = MongoClient('mongodb://localhost:27017/')
+db = client.reddit_new_db
+reddit_new_db = db.reddit_new_db
+
+working = True
+check_delay = 7200
+html = 'http://www.reddit.com'
+
+while working == True:
+    reddit_posts = Reddit_Scrape(html)
+    scrape_time = datetime.datetime.now()
+    reddit_dict = reddit_posts.main_loop()
+    for post,content in reddit_dict.items():
+        reddit_new_db.insert_one({
+            'post': post,
+            'info': content,
+            'time': scrape_time
+        })
+    time.sleep(check_delay + int(random.random()*100))
